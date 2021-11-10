1eafa1b088d19b932af04adbc902d242774d2ef9
Sam Parris <Sam.Parris@outlook.com>
2021-11-10 10:20:03 +0000

    Initial commit for Disable-ADUser.ps1

---
+++ Functions/Public/Disable-ADUser.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Disables AD User.
+.DESCRIPTION
+    This function disables a specified AD Account aswell as performing the following;
+    - Moves User to the 'Disabled OU' within Active Directory
+.NOTES
+    AUTHOR: Sam Parris
+    CREATION DATE: 10-November-2021
+#>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
+param()
+
+Function SysAdmin.Disable-ADUser {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)] $UserName
+    )
+    Begin {
+        $ErrorActionPreference = 'Stop'
+        $DisabledOU = "OU=Obj-Users,OU=Generic-Disabled Accounts,OU=ORG,DC=invotec-uk,DC=com"
+    }
+    Process {
+        Try {
+            $AllUserInfo = Get-ADUser -Properties * -Identity $UserName
+        } Catch {
+            Write-Error "$($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+        }
+        If ($AllUserInfo.Enabled -eq $false) {
+            Write-Error "$($UserName) is already disabled within Active Directory."
+        }
+    }
+}
